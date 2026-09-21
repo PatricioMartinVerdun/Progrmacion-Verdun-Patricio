cdae19260edfda2f16bd3534d8759e7226a0164f
PatricioMartinVerdun <verdunpatriciosagrado@gmail.com>
2026-09-21 14:05:07 -0300

    Add files via upload

---
+++ strings_array.php
@@ -0,0 +1,10 @@
+<?php
+
+$fecha_1="2010/03/05";
+$fecha_2="2021-03-08";
+$numeros="Uno Dos Tres Cuatro Cinco Seis Siete";
+
+$array_fecha=explode ("-",$fecha_2);
+
+echo $array_fecha [0];
+echo $array_fecha [2];
